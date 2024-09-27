26f41630b598a814c1336569e1232a9a2223fafc
Hoang Tien <tienqhwork@gmail.com>
2024-09-27 20:41:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Set the date we're counting down to
+
+// Set the countdown date to tomorrow's date
+var countDownDate = new Date("Sep 29 2024 15:00:00 GMT+0700").getTime();
+// Update the count down every 1 second
+var countdownfunction = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in the elements with id="days", "hours", "minutes", "seconds"
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.querySelector(".countdown").innerHTML =
+      "Maintenance is completed!";
+  }
+}, 1000);
